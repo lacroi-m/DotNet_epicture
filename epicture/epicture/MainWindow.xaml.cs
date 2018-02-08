7f12773ec54aa47b5d7c5abcaec1df7662196131
rollan_i <cyprien.rolland@epitech.eu>
2018-02-08 20:51:20 +0100

    loading image fickr

--- epicture/epicture/MainWindow.xaml.cs
+++ epicture/epicture/MainWindow.xaml.cs
@@ -28,48 +28,47 @@ namespace epicture
             StackPanel myStackPanel = new StackPanel();
 
             //Create TextBox
-            TextBox myTextBox = new TextBox();
-            myTextBox.Width = 200;
+            //TextBox myTextBox = new TextBox();
+           // myTextBox.Width = 200;
 
             
             // Enable spellchecking on the TextBox.
-            myTextBox.SpellCheck.IsEnabled = true;
+         //   myTextBox.SpellCheck.IsEnabled = true;
 
             // Alternatively, the SetIsEnabled method could be used
             // to enable or disable spell checking like this:
             // SpellCheck.SetIsEnabled(myTextBox, true);
 
-            myStackPanel.Children.Add(myTextBox);
-            this.Content = myStackPanel;
+           // myStackPanel.Children.Add(myTextBox);
+           // this.Content = myStackPanel;
         }
     }
 
     public partial class MainWindow : Window
     {
+        string search;
+        string str = "http://qnimate.com/wp-content/uploads/2014/03/images2.jpg";
+        List<string> url = new List<string>();
+        int list = 0;
         const string apiKey = "1b83043f069825ebe46685c2a1ba4bc9";
         const string password = "ed73e0b97611e032";
 
 
         Flickr flickr = new Flickr(apiKey);
         public MainWindow()
-        {
+        { 
 
-            //const string apiKey = "yourapikeygoeshere";
-
-            //Flickr flickr = new Flickr(apiKey);
 
 
         }
-        private void TextBlock_Loaded(object sender, RoutedEventArgs e)
+
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-
-
-            string searchTerm = "sexy";
-
+            search = MyTextBox.Text;
             var options = new PhotoSearchOptions
             {
-                Tags = searchTerm,
-                PerPage = 10000,
+                Tags = search,
+                PerPage = 1,
                 Page = 1
             };
             PhotoCollection photos = flickr.PhotosSearch(options);
@@ -77,31 +76,45 @@ namespace epicture
             // Get TextBlock reference.
             var block = sender as TextBlock;
             // Set text.
-            var line = 1;
+            int line = 1;
             //list contenant les urls des images à charger (chercher en fcts des tags)
-            List<string> url = new List<string>();
 
             TextBlock textBlock = new TextBlock();
 
             foreach (Photo photo in photos)
             {
+                var Path = photo.WebUrl;
+                var image = new Image();
+
+                BitmapImage myBitmapImage = new BitmapImage();
+                myBitmapImage.BeginInit();
+                myBitmapImage.UriSource = new Uri(photo.WebUrl, UriKind.Absolute);
+                myBitmapImage.EndInit();
+                image.Source = myBitmapImage;
                 url.Add(photo.WebUrl);
+                MyTextBox.Text = photo.Farm;
                 //appeler la fonction qui charge les images via l'url
                 textBlock.Name = "yo";
-
                 //du coup ça ça affiche le dernier url.
-                block.Text = photo.WebUrl;
                 line++;
+                // MyLabel.Content = MyTextBox.Text;
+            }
+        }
 
+        private void TextBlock_Loaded(object sender, RoutedEventArgs e)
+        {
+
+
+
+            
                 //s'occper des favoris
 
                 //s'occuper de imgbur (sevag inc rush tmtc)
                 //s'occuper de l'UX
-            }
+          
             /*block.Text = "This is some longer text in the TextBlock. " +
                 "We see how it wraps in this example. " +
                 "TextBlock is meant for longer text.";*/
         }
-
     }
 };
